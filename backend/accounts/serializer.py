bbb08a20df292642b9e69d2fa2fb0b8577575974
Nicksheinman <nicksheinman@gmail.com>
2026-04-27 19:52:52 +0300

    starting_on_registartion_view

---
+++ backend/accounts/serializer.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import User
+
+
+
+class CustomerRegistrationSerializer(serializers.Serializer):
+    username=serializers.CharField(max_length=150)
+    email=serializers.EmailField()
+    password=serializers.CharField(write_only=True)
+    second_password=serializers.CharField(write_only=True)
+    first_name=serializers.CharField(required=True, allow_blank=False)
+    last_name=serializers.CharField(required=False, allow_blank=True)
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("username exist")
+        return value
+    def validate(self, attrs):
+        if attrs['password']!=attrs['second_password']:
+            raise serializers.ValidationError('password do not match')
+        return attrs
+    def validate_email(self, value):
+        if User.objects.filter(email__iexact=value).exists():
+            raise serializers.ValidationError("User with this email already exists.")
+        return value
+    def create(self, validated_data):
+        validated_data.pop('second_password')
+        user=User(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            role=User.role.CUSTOMER
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+    
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializer import CustomerRegistrationSerializer
 
-# Create your views here.
+
+# class CustomerRegistrationView(APIView):
+    
+#     def post(self, request):
+#         serializer=CustomerRegistrationSerializer
+        
+#         if serializer.is_valid():
+            
+
+# class registr
