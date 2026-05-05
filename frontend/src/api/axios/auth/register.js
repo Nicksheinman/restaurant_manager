68c4d426451b8f3b56ffebceb6ee5277c2f02975
Nicksheinman <nicksheinman@gmail.com>
2026-05-05 22:04:29 +0300

    frontend_start1

---
+++ frontend/src/api/axios/auth/register.js
@@ -0,0 +1,10 @@
+import api  from "../../api";
+
+const registerCustomerApi= async (username, password, secondPassword, email, firstName, lastName) => {
+    try {
+        api.post()
+    }
+    catch {
+
+    }
+}
